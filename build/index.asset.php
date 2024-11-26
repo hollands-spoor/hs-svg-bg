1aee7d7a32540b338ad2ea2d4c181aaadb9ba4b3
hollands-spoor <166383984+hollands-spoor@users.noreply.github.com>
2024-11-26 15:04:19 +0100

    Build made

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '02436559a939c294fe21');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'af7a51526c6c1507d6de');
